19a71d05be72d4e9bdba1c7c6abd0e9a9d9d4899
杨魁 <yangkui.thoamsy@bytedance.com>
2019-10-27 15:07:12 +0800

    feat: 批处理的测试

--- App.jsx
+++ App.jsx
@@ -1,4 +1,10 @@
-import React, { useState, Suspense } from 'react';
+import React, {
+  useState,
+  Suspense,
+  useTransition,
+  useEffect,
+  useDeferredValue,
+} from 'react';
 
 import { fetchProfileData } from './fakeAPI';
 
@@ -7,15 +13,34 @@ const initialResource = fetchProfileData(0);
 
 function App() {
   const [resource, setResource] = useState(initialResource);
+  const [count, setCount] = useState(0);
+  const [startTransition, isPending] = useTransition({ timeout: 1000 });
+
+  useEffect(() => {
+    // 因为新的 Render 模式，会将所有的 setState 都批处理。所以 legacy 模式下，每次 click 这里会输出 5 次
+    // 而新的 Concurrent or BlockRender 模式下，都只是输出一次。
+    console.count('update');
+  }, [count]);
   return (
     <>
       <button
+        type="button"
+        disabled={isPending}
         onClick={() => {
-          const nextUserId = getNextId(resource.userId);
-          setResource(fetchProfileData(nextUserId));
+          startTransition(() => {
+            const nextUserId = getNextId(resource.userId);
+            setResource(fetchProfileData(nextUserId));
+            setTimeout(() => {
+              setCount(c => c + 1);
+              setCount(c => c + 1);
+              setCount(c => c + 1);
+              setCount(c => c + 1);
+              setCount(c => c + 1);
+            });
+          });
         }}
       >
-        Next
+        {isPending ? 'loading…' : 'Next'}
       </button>
       <ProfilePage resource={resource} />
     </>
@@ -29,7 +54,30 @@ function ProfilePage({ resource }) {
       <Suspense fallback={<h1>Loading posts...</h1>}>
         <ProfileTimeline resource={resource} />
       </Suspense>
+      <Suspense fallback="Loading a slow list…">
+        <SlowList />
+      </Suspense>
     </Suspense>
+  );
+}
+
+function SlowList({ length = 30 }) {
+  const [value, setValue] = useState('');
+  return (
+    <>
+      <input
+        type="text"
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      />
+      <ul>
+        {Array.from({ length }, (_, i) => {
+          const end = Date.now() + 5;
+          while (Date.now() < end) {}
+          return <li key={i}>{value}</li>;
+        })}
+      </ul>
+    </>
   );
 }
 
